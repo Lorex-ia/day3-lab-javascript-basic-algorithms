63cfcbf517c82eb5b535b1f4bdfffb9bc8f4c372
Lorexia <alexia.lorenza.martinel@protonmail.com>
2023-01-11 13:19:01 +0100

    pushing iteration3

--- index.js
+++ index.js
@@ -33,3 +33,36 @@ else {
 
 
 // Iteration 3: Loops
+/* 3.1 Print the characters of the driver's name, separated by space, and in capital letters, i.e., "J O H N".
+
+3.2 Print all the characters of the navigator's name, in reverse order, i.e., "nhoJ".
+
+3.3 Depending on the lexicographic order of the strings, print:
+
+    The driver's name goes first.
+    Yo, the navigator goes first definitely.
+    What?! You both have the same name? */
+const driverName = "Silur";
+const navigatorName = "Lorexiam";
+
+
+let transfoDriver = driverName.toUpperCase();
+console.log(transfoDriver);
+
+
+let transfoNavigator = navigatorName.split('').reverse().join('');
+console.log(transfoNavigator);
+
+let comparisionName = transfoDriver.localeCompare(transfoNavigator);
+
+if (comparisionName <0){
+  console.log("The drivers name goes first!")
+}
+
+else if (comparisionName>0){
+  console.log("The navigators name goes first!")
+}
+
+else{
+  console.log("What, you both have the same name?!")
+};
